26b570d28772b54ff63ff8a19980d7e895e53090
zitaharry <harrymuigua@gmail.com>
2025-10-02 18:49:25 +0300

    Implement Header

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Signalist logo"
+            width={140}
+            height={32}
+            className="h-8 w-auto cursor-pointer"
+          />
+        </Link>
+        <nav className="hidden sm:block">
+          <NavItems />
+        </nav>
+
+        <UserDropdown />
+      </div>
+    </header>
+  );
+};
+export default Header;
---
+++ components/NavItems.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { NAV_ITEMS } from "@/lib/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    if (path === "/") return pathname === "/";
+
+    return pathname.startsWith(path);
+  };
+
+  return (
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+      {NAV_ITEMS.map(({ href, label }) => (
+        <li key={href}>
+          <Link
+            href={href}
+            className={`hover:text-yellow-500 transition-colors ${
+              isActive(href) ? "text-gray-100" : ""
+            }`}
+          >
+            {label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default NavItems;
