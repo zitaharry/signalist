791de0640d4444639939a9fdab75b07991e983e8
zitaharry <harrymuigua@gmail.com>
2025-10-04 15:39:38 +0300

    Implement Stock Search, Stock Details & Partial Watchlist System

---
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -0,0 +1,73 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
+import WatchlistButton from "@/components/WatchlistButton";
+import {
+  SYMBOL_INFO_WIDGET_CONFIG,
+  CANDLE_CHART_WIDGET_CONFIG,
+  BASELINE_WIDGET_CONFIG,
+  TECHNICAL_ANALYSIS_WIDGET_CONFIG,
+  COMPANY_PROFILE_WIDGET_CONFIG,
+  COMPANY_FINANCIALS_WIDGET_CONFIG,
+} from "@/lib/constants";
+
+export default async function StockDetails({ params }: StockDetailsPageProps) {
+  const { symbol } = await params;
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
+  return (
+    <div className="flex min-h-screen p-4 md:p-6 lg:p-8">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
+        {/* Left column */}
+        <div className="flex flex-col gap-6">
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}symbol-info.js`}
+            config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
+            height={170}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}advanced-chart.js`}
+            config={CANDLE_CHART_WIDGET_CONFIG(symbol)}
+            className="custom-chart"
+            height={600}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}advanced-chart.js`}
+            config={BASELINE_WIDGET_CONFIG(symbol)}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+
+        {/* Right column */}
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center justify-between">
+            <WatchlistButton
+              symbol={symbol.toUpperCase()}
+              company={symbol.toUpperCase()}
+              isInWatchlist={false}
+            />
+          </div>
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}technical-analysis.js`}
+            config={TECHNICAL_ANALYSIS_WIDGET_CONFIG(symbol)}
+            height={400}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}company-profile.js`}
+            config={COMPANY_PROFILE_WIDGET_CONFIG(symbol)}
+            height={440}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}financials.js`}
+            config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)}
+            height={464}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -47,7 +47,7 @@ const UserDropdown = ({ user }: { user: User }) => {
         <DropdownMenuLabel>
           <div className="flex relative items-center gap-3 py-2">
             <Avatar className="h-10 w-10">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+              <AvatarImage src="https://avatars.githubusercontent.com/u/120082371?s=400&v=4" />
               <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                 {user.name[0]}
               </AvatarFallback>
